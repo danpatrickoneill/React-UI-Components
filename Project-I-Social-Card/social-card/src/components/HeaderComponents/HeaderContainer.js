d97233804b794b0444ffc1384af962b162f784f2
danpatrickoneill <dan47573@gmail.com>
2019-03-18 18:05:48 -0400

    Got header and card looking about right

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+  return (
+    <header className="header-container">
+      <ImageThumbnail />
+      <div className="header-text">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </header>
+  );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+// var moment = require("moment");
+// moment().format();
+
+// var now = moment();
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <p>Lambda School</p>
+      <p>@LambdaSchool</p>
+      <p>- jan 26</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
