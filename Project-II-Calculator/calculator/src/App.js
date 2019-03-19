4c497c6e23ab63a7647fd0baa32130ca56889267
danpatrickoneill <dan47573@gmail.com>
2019-03-19 16:45:08 -0400

    Got all elements layed out in grid

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calc-container">
+      <CalculatorDisplay />
+
+      <ActionButton className="btn clear-btn" symbol="clear" />
+      <ActionButton className="btn divide-btn" symbol="÷" />
+      <ActionButton className="btn multiply-btn" symbol="x" />
+      <ActionButton className="btn subtract-btn" symbol="-" />
+      <ActionButton className="btn add-btn" symbol="+" />
+      <ActionButton className="btn equals-btn" symbol="=" />
+      <NumberButton className="btn" number="1" />
+      <NumberButton className="btn" number="2" />
+      <NumberButton className="btn" number="3" />
+      <NumberButton className="btn" number="4" />
+      <NumberButton className="btn" number="5" />
+      <NumberButton className="btn" number="6" />
+      <NumberButton className="btn" number="7" />
+      <NumberButton className="btn" number="8" />
+      <NumberButton className="btn" number="9" />
+      <NumberButton className="btn zero-btn" number="0" />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const ActionButton = props => {
+  return <div className={props.className}>{props.symbol}</div>;
+};
+
+export default ActionButton;
